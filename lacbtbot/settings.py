30b2d534a1bace7351fe3767461b4cfa2bf1f5ac
ahmadore <ahmadagaie@gmail.com>
2017-04-29 00:41:59 +0100

    m

--- lacbtbot/settings.py
+++ lacbtbot/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'lacbtbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-SECRET_KEY = config('SECRET_KEY')
+
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
